2b9193241a8d20635115abf7e5e6e60b83b96635
Hau Dinh <Haudreamer2104@gmail.com>
2024-10-10 15:19:21 +0700

    revert merge feats/cop

--- team8/components/customs ui/Loader.tsx
+++
@@ -1,8 +0,0 @@
-const Loader = () => {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full border-t-4 border-bg-main border-solid h-13 w-13"></div>
-      </div>
-    );
-  }
-  export default Loader;
