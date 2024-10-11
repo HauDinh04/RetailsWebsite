32ffa5d9ccb796dbaddc288b3f2d9121ba86146e
NnKha <nhatkha2018@gmail.com>
2024-10-12 00:15:58 +0700

    UI component Slide-Show

--- team8/app/(root)/test/page.tsx
+++ team8/app/(root)/test/page.tsx
@@ -1,12 +1,11 @@
+import Slideshow from "@/components/layouts/SlideShow";
 import React from "react";
 
 function Page() {
   return (
     <div>
-      <h1 className={"text-center"}>
-     
-        Đây là trang test
-      </h1>
+      <h1 className={"text-center"}>Đây là trang test</h1>
+      <Slideshow></Slideshow>
     </div>
   );
 }
--- team8/components/layouts/SlideShow.tsx
+++ team8/components/layouts/SlideShow.tsx
@@ -3,42 +3,50 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/scrollbar";
-import "swiper/css/autoplay";
-import { Scrollbar, Autoplay } from "swiper/modules";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 import Image from "next/image";
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
 
 const images = [
-  "/slideshow/slide1.jpg",
-  "/slideshow/slide2.jpg",
-  "/slideshow/slide4.jpg",
-  "/slideshow/slide5.jpg",
-  "/slideshow/slide3.png",
-  "/slideshow/slide6.png",
-  "/slideshow/slide7.png",
+  "/slideshow/slider-2.jpg",
+  "/slideshow/slider-2.jpg",
+  "/slideshow/slider-2.jpg",
 ];
 
 export default function Slideshow() {
   return (
-    <div className="flex justify-center items-center mt-2 rounded-md object-cover">
+    <div className="container w-[847px] h-[414px] py-[15px] relative">
       <Swiper
-        scrollbar={{ hide: true }}
-        autoplay={{ delay: 7000, disableOnInteraction: false }}
-        modules={[Scrollbar, Autoplay]}
-        className="mySwiper"
-        style={{ width: "880px", height: "430px" }}
+        cssMode={true}
+        navigation={{
+          nextEl: ".button-next-slide",
+          prevEl: ".button-prev-slide",
+        }}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper "
       >
-        {images.map((src, index) => (
-          <SwiperSlide key={index} style={{ width: "100%", height: "100%" }}>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
             <Image
-              src={src}
-              alt={`slide ${index + 1}`}
-              width={880}
-              height={430}
-              layout="responsive"
+              src={image}
+              alt={`slide-${index}`}
+              width={847}
+              height={414}
             />
           </SwiperSlide>
         ))}
+        <div className="button-next-slide rounded	bg-[#9f9a9a] w-[45px] h-[42px] flex items-center justify-center absolute top-[46%] right-0 z-30 hover:bg-[#0083c1] cursor-pointer">
+          <MdOutlineNavigateNext className="w-[25px] h-[30px] text-white-1" />
+        </div>
+        <div className="button-prev-slide rounded 	bg-[#9f9a9a] w-[45px] h-[42px] flex items-center justify-center absolute  top-[46%] left-0 z-30 hover:bg-[#0083c1] cursor-pointer">
+          <GrFormPrevious className="w-[25px] h-[30px] text-white-1" />
+        </div>
       </Swiper>
     </div>
   );
