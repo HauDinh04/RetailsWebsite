2214a7582c97ea261a4bf15c231f61af97e262c5
NnKha <nhatkha2018@gmail.com>
2024-10-12 00:37:39 +0700

    UI component Slide-Show

--- team8/app/(root)/test/page.tsx
+++ team8/app/(root)/test/page.tsx
@@ -1,11 +1,9 @@
-import Slideshow from "@/components/layouts/SlideShow";
 import React from "react";
 
 function Page() {
   return (
     <div>
       <h1 className={"text-center"}>Đây là trang test</h1>
-      <Slideshow></Slideshow>
     </div>
   );
 }
