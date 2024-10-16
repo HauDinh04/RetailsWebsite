447273632bed7532f1f90ee44916498201abef5c
NnKha <nhatkha2018@gmail.com>
2024-10-17 00:20:43 +0700

    reponsive ui footer

--- team8/components/layouts/Footer.tsx
+++ team8/components/layouts/Footer.tsx
@@ -14,17 +14,17 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="text-xs bg-[#222222] text-[#fff] pt-[55px] ">
-      <div className="container ">
+      <div className="container">
         <div className="w-full flex flex-wrap pb-[21px]">
-          <div className="w-2/5">
+          <div className="md:w-2/5 md:shrink w-full">
             <h3 className="text-[18px] uppercase font-semibold	after:content-[''] after:block after:w-10 after:h-0.5 after:bg-blue-500 after:mt-4 after:mb-5">
               Contact Us
             </h3>
             <div>
               <ul>
                 <li>
-                  <p className="flex items-center text-footer-info text-caption font-normal">
-                    <IoLocationSharp className="w-3 h-4 mr-[10px]" />
+                  <p className="text-footer-info text-caption font-normal">
+                    <IoLocationSharp className="w-3 h-4 mr-[10px] inline" />
                     <span className="font-medium pr-2 text-white-1 	">
                       Address :
                     </span>
@@ -32,8 +32,8 @@ export default function Footer() {
                   </p>
                 </li>
                 <li>
-                  <p className="flex items-center text-footer-info text-caption font-normal">
-                    <MdOutlineMailOutline className="w-3 h-3 mr-[10px]" />
+                  <p className=" text-footer-info text-caption font-normal">
+                    <MdOutlineMailOutline className="w-3 h-3 mr-[10px] inline" />
                     <span className="text-xs font-medium pr-2 text-white-1 	">
                       Email :
                     </span>
@@ -46,8 +46,8 @@ export default function Footer() {
                   </p>
                 </li>
                 <li>
-                  <p className="flex items-center text-footer-info text-caption font-normal">
-                    <FaPhoneAlt className="w-2 h-3 mr-[10px]" />
+                  <p className=" text-footer-info text-caption font-normal">
+                    <FaPhoneAlt className="w-2 h-3 mr-[10px] inline" />
                     <span className="text-xs font-medium pr-2 text-white-1 	">
                       Phone :
                     </span>
@@ -81,7 +81,7 @@ export default function Footer() {
               </ul>
             </div>
           </div>
-          <div className="w-1/5">
+          <div className="md:w-1/5 md:shrink w-full">
             <h3 className="text-[18px] uppercase font-semibold	after:content-[''] after:block after:w-10 after:h-0.5 after:bg-blue-500 after:mt-4 after:mb-5">
               Information
             </h3>
@@ -111,7 +111,7 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="w-1/5">
+          <div className="md:w-1/5 md:shrink w-full">
             <h3 className="text-[18px] uppercase font-semibold	after:content-[''] after:block after:w-10 after:h-0.5 after:bg-blue-500 after:mt-4 after:mb-5">
               Extras
             </h3>
@@ -140,7 +140,7 @@ export default function Footer() {
               </ul>
             </div>
           </div>
-          <div className="w-1/5">
+          <div className="md:w-1/5 md:shrink w-full">
             <h3 className="text-[18px] uppercase font-semibold	after:content-[''] after:block after:w-10 after:h-0.5 after:bg-blue-500 after:mt-4 after:mb-5 hover:transform hover:translate-x-2 transition-transform duration-500 hover:text-bg-main">
               My Account
             </h3>
@@ -176,10 +176,11 @@ export default function Footer() {
           </div>
         </div>
       </div>
+
       <div className="w-full bg-[#181818] pt-[25px] pb-[18px]">
         <div className="container">
-          <div className=" flex  ">
-            <div className="w-1/2	">
+          <div className=" flex flex-wrap ">
+            <div className="md:w-1/2 w-full md:text-start text-center pb-5	">
               <h3 className="text-[20px] uppercase mb-[10px] text-[#0083c1]  font-semibold">
                 Need Help ?
               </h3>
@@ -187,20 +188,19 @@ export default function Footer() {
                 SUPPORT TEAM 24/7 AT (844) 555-8386
               </p>
             </div>
-
             <form
               action=""
-              className="w-1/2  relative flex items-center justify-between  space-x-2"
+              className="flex-col text-end gap-[30px] md:gap-[15px]  px-[15px] lg:px-0 md:flex-row justify-center md:w-1/2 w-full relative flex items-center md:justify-between"
             >
               <Input
                 type="email"
                 placeholder="Your Email Address..."
-                className="text-[12px] w-4/5 h-[45px] pl-[55px] rounded-[3px] text-black"
+                className="md:w-4/5 w-full text-[12px] h-[45px] md:ml-[12px] md:pl-[55px]  rounded-[3px] text-black "
               />
-              <MdEmail className="absolute left-2 text-[#999999] bg-white" />
+              <MdEmail className="hidden md:block absolute md:left-10 md:top-8  text-[#999999] bg-white " />
 
               <Button
-                className="uppercase  text-[12px] h-[45px] py-1.5	px-3 font-normal rounded-[3px] bg-[#0083c1] hover:bg-[#0083c1] "
+                className="uppercase mb-[25px] md:mb-0 text-center text-[12px] h-[45px] md:py-1.5	md:px-3 font-normal rounded-[3px] bg-[#0083c1] hover:bg-[#0083c1] "
                 type="submit"
               >
                 Subscribe
@@ -210,13 +210,13 @@ export default function Footer() {
         </div>
       </div>
       <div>
-        <div className="container  py-[32px]">
-          <div className="flex">
-            <div className="w-5/12	 flex items-center justify-between">
-              <h3 className="text-[18px] uppercase mb-[10px] leading-10  font-medium">
+        <div className="container py-[32px]">
+          <div className="flex flex-wrap">
+            <div className="lg:w-5/12 lg:pb-0 md:pb-0 pb-5 w-full flex flex-wrap items-center lg:justify-between justify-center">
+              <h3 className="hidden lg:block  text-[18px] uppercase mb-[10px] leading-10  font-medium">
                 Download Our App
               </h3>
-              <div className="relative aspect-square w-[119px] h-[40px] rounded-sm">
+              <div className="mr-[10px] lg:mr-0 relative aspect-square w-[119px] h-[40px] rounded-sm">
                 <Image
                   src="/images/ggplay.jpg"
                   alt=""
@@ -233,7 +233,7 @@ export default function Footer() {
                 />
               </div>
             </div>
-            <div className="w-7/12 flex flex-row justify-end ">
+            <div className="lg:w-7/12 lg:pb-0 md:pb-0 pb-5 w-full flex flex-wrap flex-row lg:justify-end  justify-center">
               <div className="relative aspect-square w-[52px] h-[32px]  ml-[7px]">
                 <Image
                   src="/images/visa.jpg"
@@ -284,7 +284,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
-          <div className="flex flex-row justify-end text-[12px] mt-[5px]">
+          <div className="flex flex-wrap  py-8 flex-row lg:justify-end justify-center text-[12px] mt-[5px]">
             <p>
               Destino - Premium Multipurpose HTML5/CSS3 Theme - Designed by{" "}
             </p>
