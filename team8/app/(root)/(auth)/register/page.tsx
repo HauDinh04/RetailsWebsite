51e85f117f7cbd1eb11eb08774e0b5ed8d9a21a5
vuvansy <vuvansy3001@gmail.com>
2024-10-21 22:20:29 +0700

    Fix bug layout Register

--- team8/app/(root)/(auth)/register/page.tsx
+++ team8/app/(root)/(auth)/register/page.tsx
@@ -5,31 +5,34 @@ import Breadcrumb from "@/components/layouts/Breadcrumb";
 
 export default function Register() {
     return (
-        <>
-            <Breadcrumb
-                breadcrumbItems={[
-                    {
-                        label: "Account",
-                        link: "/register",
-                    },
-                    {
-                        label: "Register",
-                        link: "#",
-                    },
-                ]}
-            />
-            <div className="mb-[23px]">
-                <h2 className="text-[#222222] text-[30px] font-light mt-[20px] mb-[10px]">
-                    Register Account
-                </h2>
-                <p className="mb-[10px] text-black-2 text-info">
-                    If you already have an account with us, please login at the
-                    <Link href={""} className="hover:text-bg-main">
-                        login page
-                    </Link>
-                </p>
-                <RegisterForm />
+        <div className="mx-[-15px]">
+            <div className="px-[15px]">
+                <Breadcrumb
+                    breadcrumbItems={[
+                        {
+                            label: "Account",
+                            link: "/register",
+                        },
+                        {
+                            label: "Register",
+                            link: "#",
+                        },
+                    ]}
+                />
+                <div className="mb-[23px]">
+                    <h2 className="text-[#222222] text-[30px] font-light mt-[20px] mb-[10px]">
+                        Register Account
+                    </h2>
+                    <p className="mb-[10px] text-black-2 text-info">
+                        If you already have an account with us, please login at
+                        the
+                        <Link href={""} className="hover:text-bg-main">
+                            login page
+                        </Link>
+                    </p>
+                    <RegisterForm />
+                </div>
             </div>
-        </>
+        </div>
     );
 }
