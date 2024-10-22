5f48979008e6aad814ba4b4e9da7b59b59467462
Hau Dinh <Haudreamer2104@gmail.com>
2024-10-22 13:43:37 +0700

    deleted fivetar

--- components/pages/category/SmallHorizontalBoxProduct.tsx
+++ components/pages/category/SmallHorizontalBoxProduct.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import {FaStar, FaStarHalfAlt} from "react-icons/fa";
+import {FaStar} from "react-icons/fa";
 
 function SmallHorizontalBoxProduct({id, name, rating, price, image, oldPrice, className = ''}: {
     id: string,
