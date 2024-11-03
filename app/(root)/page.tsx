81a437bf43d1231354186964cfcf7ee936f2d86b
Hau Dinh <Haudreamer2104@gmail.com>
2024-11-03 23:51:19 +0700

    hompage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,20 +12,28 @@ import FeatureProduct from "@/components/layouts/FeatureProduct";
 import Slideshow from "@/components/layouts/SlideShow";
 import { CountDownProduct } from "@/components/layouts/CountDownProduct";
 import Image from "next/image";
-import { FreeMode, Pagination } from "swiper/modules";
+import {
+  Autoplay,
+  FreeMode,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
 import SmallBoxProduct from "@/components/layouts/SmallBoxProduct";
 import Link from "next/link";
+
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
 export default function Home() {
   // /  const [isShow, setIsShow] = useState(true);
 
   return (
     <div className="items-center justify-center py-[10px] mb-4 ">
-      <div className="mx-[-15px] items-center w-full  flex justify-between  relative">
-        <div className=""><LeftNav /></div>
+      <div className=" mx-[-15px] items-center w-full  flex justify-between  relative">
+        <div className="px-[15px]">
+          <LeftNav />
+        </div>
         <Slideshow />
-          
-         
-          
       </div>
       <div className="mt-4">
         <div className="py-[10px] grid grid-cols-1 md:grid-cols-3  ">
@@ -100,71 +108,71 @@ export default function Home() {
         </div>
       </div>
       <section className="xl:block mt-10 md:hidden">
-      <div className="flex gap-4 object-cover">
-        <Link href={'/'}>
-          <div className="relative group overflow-hidden w-[578px] h-[200px]">
-            <Image
-              src={'/banner/banner1.jpg'}
-              alt="banner"
-              layout="fill"
-              objectFit="cover"
-              className="transition-transform duration-500 group-hover:scale-105"
-            />
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
-          </div>
-        </Link>
-        <Link href={'/'}>
-          <div className="relative group overflow-hidden w-[578px] h-[200px]">
-            <Image
-              src={'/banner/banner1.jpg'}
-              alt="banner"
-              layout="fill"
-              objectFit="cover"
-              className="transition-transform duration-500 group-hover:scale-105"
-            />
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
-          </div>
-        </Link>
-      </div>
-      <div className="flex gap-4 mt-4">
-        <Link href={'/'}>
-          <div className="relative group overflow-hidden w-[282px] h-[400px]">
-            <Image
-              src={'/banner/banner2.jpg'}
-              alt="banner"
-              layout="fill"
-              objectFit="cover"
-              className="transition-transform duration-500 group-hover:scale-105"
-            />
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
-          </div>
-        </Link>
-        <Link href={'/'}>
-          <div className="relative group overflow-hidden w-[578px] h-[400px]">
-            <Image
-              src={'/banner/banner3.jpg'}
-              alt="banner"
-              layout="fill"
-              objectFit="cover"
-              className="transition-transform duration-500 group-hover:scale-105"
-            />
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
-          </div>
-        </Link>
-        <Link href={'/'}>
-          <div className="relative group overflow-hidden w-[282px] h-[400px]">
-            <Image
-              src={'/banner/banner2.jpg'}
-              alt="banner"
-              layout="fill"
-              objectFit="cover"
-              className="transition-transform duration-500 group-hover:scale-105"
-            />
-            <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
-          </div>
-        </Link>
-      </div>
-    </section>
+        <div className="flex gap-4 object-cover">
+          <Link href={"/"}>
+            <div className="relative group overflow-hidden w-[578px] h-[200px]">
+              <Image
+                src={"/banner/banner1.jpg"}
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                className="transition-transform duration-500 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
+            </div>
+          </Link>
+          <Link href={"/"}>
+            <div className="relative group overflow-hidden w-[578px] h-[200px]">
+              <Image
+                src={"/banner/banner1.jpg"}
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                className="transition-transform duration-500 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
+            </div>
+          </Link>
+        </div>
+        <div className="flex gap-4 mt-4">
+          <Link href={"/"}>
+            <div className="relative group overflow-hidden w-[282px] h-[400px]">
+              <Image
+                src={"/banner/banner2.jpg"}
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                className="transition-transform duration-500 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
+            </div>
+          </Link>
+          <Link href={"/"}>
+            <div className="relative group overflow-hidden w-[578px] h-[400px]">
+              <Image
+                src={"/banner/banner3.jpg"}
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                className="transition-transform duration-500 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
+            </div>
+          </Link>
+          <Link href={"/"}>
+            <div className="relative group overflow-hidden w-[282px] h-[400px]">
+              <Image
+                src={"/banner/banner2.jpg"}
+                alt="banner"
+                layout="fill"
+                objectFit="cover"
+                className="transition-transform duration-500 group-hover:scale-105"
+              />
+              <div className="absolute inset-0 bg-gradient-to-r from-transparent bg-[#45474B] to-transparent opacity-0 group-hover:opacity-70 transition-all duration-500"></div>
+            </div>
+          </Link>
+        </div>
+      </section>
 
       <FeatureProduct />
       <div className="">
@@ -204,8 +212,9 @@ export default function Home() {
                 <Image
                   src="/images/blog-3.jpg"
                   alt="images blogs"
-                 fill
-                  
+                  layout="responsive"
+                  width={200}
+                  height={200}
                   className=" object-cover"
                 />
               </div>
@@ -218,7 +227,60 @@ export default function Home() {
           .fill("")
           .map((_, index) => (
             <div key={index}>
-              <h2 className="py-2 text-heading3-bold">New Arrivals</h2>
+              <div className="pt-[43px] relative">
+                <div className="flex flex-row items-center justify-between mb-[5px]">
+                  <h3 className="text-[#222222] text-heading3-bold">
+                    New Arrivals
+                  </h3>
+                  <div className="flex flex-row">
+                    <div className="w-[30px] h-[30px] bg-[#f2f2f2] group hover:bg-bg-main flex items-center justify-center rounded button-prev-slide">
+                      <GrFormPrevious className="text-[#333] group-hover:text-white-1 w-[16px] h-[16px]" />
+                    </div>
+
+                    <div className="bg-[#f2f2f2] group hover:bg-bg-main w-[30px] h-[30px] flex items-center justify-center rounded ml-[5px] button-next-slide">
+                      <MdOutlineNavigateNext className="text-[#333] group-hover:text-white-1" />
+                    </div>
+                  </div>
+                </div>
+                <hr className="border-dashed border border-[#f2f2f2]" />
+                <hr className="border-dashed border border-[#f2f2f2]" />
+                <hr className="border-dashed border border-[#f2f2f2]" />
+                <div className="py-[20px]">
+                  <Swiper
+                    spaceBetween={10}
+                    slidesPerView={1}
+                    navigation={{
+                      nextEl: ".button-next-slide",
+                      prevEl: ".button-prev-slide",
+                    }}
+                    modules={[Navigation, Scrollbar, Autoplay]}
+                    scrollbar={{ hide: true }}
+                    loop={true}
+                    autoplay={{ delay: 6000, disableOnInteraction: false }}
+                    className=""
+                  >
+                    <SwiperSlide className="shrink">
+                     
+                        {[...Array(10)].map((_, index) => (
+                          <SwiperSlide
+                            key={index}
+                            className="flex items-center justify-center"
+                          >
+                            <div className="w-full h-auto relative">
+                              <Image
+                                src="/images/blog-3.jpg"
+                                alt="images blogs"
+                                fill
+                                className=" object-cover"
+                              />
+                            </div>
+                          </SwiperSlide>
+                        ))}
+                      
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+              </div>
               {[...Array(3)].map((_, idx) => (
                 <SmallBoxProduct
                   key={`${index}-${idx}`}
