8cf9432e7d3c8f48f3683857e08746ad8ef048c4
Tiến Nguyễn <ngamingyahoo@gmail.com>
2024-11-05 23:07:07 +0700

    test api v1

--- app/(root)/test/page.tsx
+++ app/(root)/test/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import {fetchOrders} from "@/lib/Categories.action";
 
-function Page() {
-  return (
-    <div>
-      <h1 className={"text-center"}>
-
-      </h1>
-    </div>
-  );
+async function Page() {
+    const orders: { data: OrderType[] } = await fetchOrders();
+    return (
+        <div>
+            <h1 className={"text-center"}>
+                {orders.data.map((item, index) => (
+                    <div key={index}>
+                        <div className="">{item.order_code}</div>
+                        <div className="">{item.name}</div>
+                    </div>
+                ))}
+            </h1>
+        </div>
+    );
 }
+
 export default Page;
--- lib/Categories.action.ts
+++ lib/Categories.action.ts
@@ -1,9 +1,41 @@
 export const fetchOrders = async () => {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/order?page=1`);
-        return res.json();
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/order?page=1`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": 'Bearer c3f72a381e7f676c21b7fca43fbe60a99aa5ff5dfc76b75993da7bd3032e3f9f'
+            }
+        });
+
+        if (!res.ok) {
+            console.log(`Error: ${res.status} ${res.statusText}`);
+        }
+
+        return await res.json();
     } catch (e) {
         console.error('Error fetching orders...', e);
+        return null; // or an empty array/object as a fallback
     }
 }
 
+export const fetchCategories = async () => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": 'Bearer c3f72a381e7f676c21b7fca43fbe60a99aa5ff5dfc76b75993da7bd3032e3f9f'
+            }
+        });
+
+        if (!res.ok) {
+            console.log(`Error: ${res.status} ${res.statusText}`);
+        }
+
+        return await res.json();
+    } catch (e) {
+        console.error('Error fetching orders...', e);
+        return null;
+    }
+}
