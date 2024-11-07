c38e7cc1123942088e1419a882641ffcd0be84c3
Tiến Nguyễn <ngamingyahoo@gmail.com>
2024-11-07 17:02:25 +0700

    v0.1

--- lib/Categories.action.ts
+++ lib/Categories.action.ts
@@ -1,11 +1,12 @@
-export const fetchOrders = async () => {
+export const fetchCategories = async () => {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/order?page=1`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/categories`, {
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
                 "Authorization": 'Bearer c3f72a381e7f676c21b7fca43fbe60a99aa5ff5dfc76b75993da7bd3032e3f9f'
-            }
+            },
+            method: "GET"
         });
 
         if (!res.ok) {
@@ -15,18 +16,41 @@ export const fetchOrders = async () => {
         return await res.json();
     } catch (e) {
         console.error('Error fetching orders...', e);
-        return null; // or an empty array/object as a fallback
+        return null;
     }
 }
 
-export const fetchCategories = async () => {
+export const fetchProductByPage = async (page: number = 1) => {
     try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/products?page=${page}`, {
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
                 "Authorization": 'Bearer c3f72a381e7f676c21b7fca43fbe60a99aa5ff5dfc76b75993da7bd3032e3f9f'
-            }
+            },
+            method: "GET"
+        });
+
+        if (!res.ok) {
+            console.log(`Error: ${res.status} ${res.statusText}`);
+        }
+
+        return await res.json();
+    } catch (e) {
+        console.error('Error fetching orders...', e);
+        return null;
+    }
+}
+
+export const fetchProductByIdCategoriesAndPage = async (idCategory: number, page: number) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products?id_category=${idCategory}&page=${page}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": 'Bearer c3f72a381e7f676c21b7fca43fbe60a99aa5ff5dfc76b75993da7bd3032e3f9f'
+            },
+            method: "GET"
         });
 
         if (!res.ok) {
---
+++ lib/Checkout.action.ts
@@ -0,0 +1,22 @@
+export const CreateOrder = async (order: OrderType) => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/order?page=1`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": 'Bearer c3f72a381e7f676c21b7fca43fbe60a99aa5ff5dfc76b75993da7bd3032e3f9f'
+            },
+            body: JSON.stringify(order),
+            method: "POST"
+        });
+
+        if (!res.ok) {
+            console.log(`Error: ${res.status} ${res.statusText}`);
+        }
+
+        return await res.json();
+    } catch (e) {
+        console.error('Error fetching orders...', e);
+        return e;
+    }
+}
