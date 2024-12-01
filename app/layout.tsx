965dfcd6221e0f695f14e8aba96c93615bcb4552
eris-vn <sonn88918@gmail.com>
2024-12-02 00:27:39 +0700

    tính năng đăng nhập

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,45 +1,48 @@
-import type {Metadata} from "next";
-import "./globals.css";
-import React from "react";
-import StoreProvider from "@/app/StoreProvider";
-import {Poppins} from "next/font/google";
-import Footer from "@/components/layouts/Footer";
-import NavBar from "@/components/layouts/NavBar";
-import Header from "@/components/layouts/Header";
-import ButtonToTop from "@/components/layouts/ButtonToTop";
-import SettingButton from "@/components/layouts/SettingButton";
-import SkinCssPopup from "@/components/layouts/SkinCssPopup";
+import type { Metadata } from 'next';
+import './globals.css';
+import React from 'react';
+import StoreProvider from '@/app/StoreProvider';
+import { Poppins } from 'next/font/google';
+import Footer from '@/components/layouts/Footer';
+import NavBar from '@/components/layouts/NavBar';
+import Header from '@/components/layouts/Header';
+import ButtonToTop from '@/components/layouts/ButtonToTop';
+import SettingButton from '@/components/layouts/SettingButton';
+import SkinCssPopup from '@/components/layouts/SkinCssPopup';
+import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-    title: "Retails Website",
-    description: "",
+  title: 'Retails Website',
+  description: ''
 };
 
 const poppins = Poppins({
-    subsets: ["latin"],
-    weight: ["300", "400", "500", "600", "700"],
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700']
 });
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <body className={poppins.className}>
-        <StoreProvider>
-            <main className="relative">
-                <Header/>
-                <NavBar/>
-                {children}
-                <Footer/>
-                <SettingButton/>
-                <ButtonToTop/>
-                <SkinCssPopup/>
+  return (
+    <html lang='en'>
+      <body className={poppins.className}>
+        <SessionProvider>
+          <StoreProvider>
+            <main className='relative'>
+              <Header />
+              <NavBar />
+              {children}
+              <Footer />
+              <SettingButton />
+              <ButtonToTop />
+              <SkinCssPopup />
             </main>
-        </StoreProvider>
-        </body>
-        </html>
-    );
+          </StoreProvider>
+        </SessionProvider>
+      </body>
+    </html>
+  );
 }
