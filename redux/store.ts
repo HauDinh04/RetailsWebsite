a54154d4400f9ebb42b3d1fdf4f10f49e08881c6
Tiến Nguyễn <114715472+1stephen37@users.noreply.github.com>
2024-12-06 10:28:32 +0700

    Revert "thêm redux cho product modal"

--- redux/store.ts
+++ redux/store.ts
@@ -1,24 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
-import demoReducer from './features/todos/demoSlice';
-import accountReducer from './features/account/account.slice';
-import skinReducer from './features/skin/skin.slice';
-import noticeReducer from './features/notice/notice.slice';
-import productModalReducer from './features/product/product_modal.slice';
+import {configureStore} from '@reduxjs/toolkit';
+import demoReducer from './features/todos/demoSlice'
+import accountReducer from './features/account/account.slice'
+import skinReducer from './features/skin/skin.slice'
+import noticeReducer from './features/notice/notice.slice'
 
 export const makeStore = () => {
-  return configureStore({
-    reducer: {
-      demo: demoReducer,
-      account: accountReducer,
-      skin: skinReducer,
-      notice: noticeReducer,
-      productModal: productModalReducer
-    }
-  });
-};
+    return configureStore({
+        reducer: {
+            demo : demoReducer,
+            account: accountReducer,
+            skin: skinReducer,
+            notice: noticeReducer
+        },
+    })
+}
 
 // Infer the type of makeStore
-export type AppStore = ReturnType<typeof makeStore>;
+export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<AppStore['getState']>;
-export type AppDispatch = AppStore['dispatch'];
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
